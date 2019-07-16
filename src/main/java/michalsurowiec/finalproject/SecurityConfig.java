4dc1ff50beb57ff9aa277bfd8d0b8aecca1e7267
michalsurowiec <michalsurowiec94@o2.pl>
2019-07-16 16:52:08 +0200

    Initial setting of Spring Security

---
+++ src/main/java/michalsurowiec/finalproject/SecurityConfig.java
@@ -0,0 +1,36 @@
+package michalsurowiec.finalproject;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/shop/**").hasAnyRole("USER", "ADMIN")
+//                .antMatchers("/admin/**").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and().formLogin()
+//                .loginPage("/login")
+                .and().logout().logoutSuccessUrl("/")
+                .permitAll();
+    }
+
+//    @Bean
+//    public SpringDataUserDetailsService customUserDetailsService() {
+//        return new SpringDataUserDetailsService();
+//    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
